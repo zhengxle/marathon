cf5528c019b964f1b8bdf22522c3e697b61e7ab4
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2017-09-19 14:47:56 -0700

    One more fix

--- ci/jepsen.sh
+++ ci/jepsen.sh
@@ -2,14 +2,12 @@
 
 set -e -o pipefail
 
-START_DIR=$(pwd)
-
 apk add openjdk8
 
 wget "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein"
 chmod +x lein
 
 cd tests/jepsen
-${START_DIR}/lein deps
+../../lein deps
 
 #./dcos-launch describe | jq .
